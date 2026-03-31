09503a34d6e6317f41724d4dfe68950100841435
Prabesh Bastola <prabeshbastola382@gmail.com>
2026-03-31 12:39:02 +0545

    switch to postgres

--- Backend/eduPul/settings.py
+++ Backend/eduPul/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = "eduPul.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'edupul_db',
+        'USER': 'edupul_user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
