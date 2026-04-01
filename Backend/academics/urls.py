25c64ca2295ee89eb8d437b5ade6c57f20c047dd
Prabesh Bastola <prabeshbastola382@gmail.com>
2026-04-01 23:04:30 +0545

    CRUD operations changed

--- Backend/academics/urls.py
+++ Backend/academics/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import ProgramViewSet, CourseViewSet, DepartmentViewSet
 
-urlpatterns=[
-    path('departments/create/', views.DepartmentViewSet.as_view({'post':'create'}), name='department-create'),
-    path('departments/<int:pk>/delete/', views.DepartmentViewSet.as_view({'delete':'remove'}), name='department-delete'),
-    path('departments/', views.DepartmentViewSet.as_view({'get':'list'}), name='department-list'),
-    path('programs/', views.ProgramViewSet.as_view({'get':'list', 'post':'create'}), name='program-list'),
-    path('programs/<int:pk>/', views.ProgramViewSet.as_view({'get':'retrieve', 'put':'update', 'patch': 'partial_update', 'delete':'destroy'}), name='program-detail'),   
-]
+router = DefaultRouter()
+router.register('programs', ProgramViewSet)
+router.register('courses', CourseViewSet)
+router.register('departments', DepartmentViewSet)
+
+urlpatterns = router.urls
+
--- Backend/academics/views.py
+++ Backend/academics/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from .serializers import DepartmentSerializer, ProgramSerializer
-from .models import Department,Program
+from .serializers import DepartmentSerializer, ProgramSerializer, CourseSerializer
+from .models import Department,Program, Course
 from accounts.permissions import IsAdminOrReadOnly
 
 
@@ -14,60 +14,14 @@ class DepartmentViewSet(ModelViewSet):
     serializer_class = DepartmentSerializer
     permission_classes = [IsAdminOrReadOnly]
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-
-        if not serializer.is_valid():
-            print("DEPARTMENT CREATE ERROR:", serializer.errors)  # 🔥 IMPORTANT DEBUG
-            return Response(serializer.errors, status=400)
-
-        self.perform_create(serializer)
-        return Response(serializer.data, status=201)
-    
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-    
-    def remove(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=204)
-
 
 class ProgramViewSet(ModelViewSet):
     queryset = Program.objects.all()
     serializer_class = ProgramSerializer
-    # permission_classes = [IsAdminOrReadOnly]
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-
-        if not serializer.is_valid():
-            print("PROGRAM CREATE ERROR:", serializer.errors)  # 🔥 IMPORTANT DEBUG
-            return Response(serializer.errors, status=400)
-
-        self.perform_create(serializer)
-        return Response(serializer.data, status=201)
+    permission_classes = [IsAdminOrReadOnly]
     
-    def list(self, request, *args, **kwargs):  
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-    
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=204)
-    
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
-        if not serializer.is_valid():
-            print("PROGRAM UPDATE ERROR:", serializer.errors)
-
-        serializer.save() 
-
-
-        return Response(serializer.data, status=200)
