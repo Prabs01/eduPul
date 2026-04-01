53615065ed00c9d06d06b9af86197239a1b47c37
Prabesh Bastola <prabeshbastola382@gmail.com>
2026-04-01 22:37:19 +0545

    Program API added

--- Backend/academics/serializers.py
+++ Backend/academics/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Student, Faculty, Course, CourseEnrollment,Department
+from .models import Student, Faculty, Course, CourseEnrollment,Department,Program
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
@@ -18,3 +18,32 @@ class DepartmentSerializer(serializers.ModelSerializer):
     def remove(self, validated_data):
         department = self.get_object()
         department.delete()
+
+
+class ProgramSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Program
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Program.objects.create(
+            name = validated_data["name"],
+            degree_type = validated_data["degree_type"],
+            duration_years = validated_data["duration_years"],
+            department = validated_data["department"]
+        )
+    
+    def list(self, validated_data):
+        return Program.objects.all()
+    
+    def destroy(self, validated_data):
+        program = self.get_object()
+        program.delete()
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.degree_type = validated_data.get("degree_type", instance.degree_type)
+        instance.duration_years = validated_data.get("duration_years", instance.duration_years)
+        instance.department = validated_data.get("department", instance.department)
+        instance.save()
+        return instance
--- Backend/academics/urls.py
+++ Backend/academics/urls.py
@@ -5,4 +5,6 @@ urlpatterns=[
     path('departments/create/', views.DepartmentViewSet.as_view({'post':'create'}), name='department-create'),
     path('departments/<int:pk>/delete/', views.DepartmentViewSet.as_view({'delete':'remove'}), name='department-delete'),
     path('departments/', views.DepartmentViewSet.as_view({'get':'list'}), name='department-list'),
+    path('programs/', views.ProgramViewSet.as_view({'get':'list', 'post':'create'}), name='program-list'),
+    path('programs/<int:pk>/', views.ProgramViewSet.as_view({'get':'retrieve', 'put':'update', 'patch': 'partial_update', 'delete':'destroy'}), name='program-detail'),   
 ]
